7fe5afdf2a0c091270ed193246030c5e27260eaa
Mikhail Latypov <hodkonem1984@gmail.com>
2024-07-03 01:23:20 +0300

    Создал класс Main c методами main и calc

--- src/main/java/ru/latypov/Main.java
+++ src/main/java/ru/latypov/Main.java
@@ -1,17 +1,61 @@
 package ru.latypov;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static String calc(String input) throws IllegalArgumentException {
+        String[] parts = input.split(" ");
+        if (parts.length != 3) {
+            throw new IllegalArgumentException("Некорректный ввод");
+        }
+
+        int num1, num2;
+        char operator;
+
+        try {
+            num1 = Integer.parseInt(parts[0]);
+            operator = parts[1].charAt(0);
+            num2 = Integer.parseInt(parts[2]);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Некорректный ввод");
+        }
+        if (num1 < 1 || num1 > 10 || num2 < 1 || num2 > 10) {
+            throw new IllegalArgumentException("Вводимые числа должны быть в диапазоне от 1 до 10");
+        }
+        int result;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    throw new IllegalArgumentException("Ошибка: деление на ноль");
+                }
+                result = num1 / num2;
+                break;
+            default:
+                throw new IllegalArgumentException("Некорректная операция");
+        }
+        return Integer.toString(result);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите арифметическое выражение (например, 2 + 1");
+        try {
+            String input = scanner.nextLine();
+            String result = calc(input);
+            System.out.println("Результат: " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
         }
     }
 }
