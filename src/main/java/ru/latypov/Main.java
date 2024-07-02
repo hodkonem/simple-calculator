0e4c9be121c1c062a82e65bea6e437f484ebbbbe
Mikhail Latypov <hodkonem1984@gmail.com>
2024-07-03 01:37:21 +0300

    добавил проверку формата входной строки, оператора и целочисленности операндов

--- src/main/java/ru/latypov/Main.java
+++ src/main/java/ru/latypov/Main.java
@@ -5,24 +5,28 @@ import java.util.Scanner;
 public class Main {
 
     public static String calc(String input) throws IllegalArgumentException {
-        String[] parts = input.split(" ");
+        String[] parts = input.split("\\s+");
         if (parts.length != 3) {
-            throw new IllegalArgumentException("Некорректный ввод");
+            throw new IllegalArgumentException("Некорректный ввод: должно быть два операнда и один оператор");
         }
 
         int num1, num2;
-        char operator;
+        String operatorStr = parts[1];
+        if (operatorStr.length() != 1 || "+-*/".indexOf(operatorStr) == -1) {
+            throw new IllegalArgumentException("Некорректный оператор");
+        }
+        char operator = operatorStr.charAt(0);
 
         try {
             num1 = Integer.parseInt(parts[0]);
-            operator = parts[1].charAt(0);
             num2 = Integer.parseInt(parts[2]);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("Некорректный ввод");
+            throw new IllegalArgumentException("Операнды должны быть целыми числами от 1 до 10");
         }
         if (num1 < 1 || num1 > 10 || num2 < 1 || num2 > 10) {
-            throw new IllegalArgumentException("Вводимые числа должны быть в диапазоне от 1 до 10");
+            throw new IllegalArgumentException("Операнды должны быть целыми числами от 1 до 10");
         }
+
         int result;
         switch (operator) {
             case '+':
@@ -45,6 +49,7 @@ public class Main {
         }
         return Integer.toString(result);
     }
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
